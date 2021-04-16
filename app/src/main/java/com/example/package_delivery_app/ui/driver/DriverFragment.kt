e9156abfb5415c9d5b1ab979d0e79acb0d436aaf
shasini274 <jshasini@yahoo.com>
2021-04-16 16:48:31 +0100

    2021/04/06_V1

--- app/src/main/java/com/example/package_delivery_app/ui/driver/DriverFragment.kt
+++ app/src/main/java/com/example/package_delivery_app/ui/driver/DriverFragment.kt
@@ -1,20 +1,17 @@
 package com.example.package_delivery_app.ui.driver
 
-
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
-import androidx.recyclerview.widget.RecyclerView
-import com.example.affirmations.adapter.ItemAdapter
 import com.example.package_delivery_app.R
-import com.example.package_delivery_app.data.Datasource
+import com.example.package_delivery_app.ui.building.BuildingFragmentDirections
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-
 
 class DriverFragment : Fragment() {
 
@@ -28,18 +25,10 @@ class DriverFragment : Fragment() {
         driverViewModel =
                 ViewModelProvider(this).get(DriverViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_driver, container, false)
-        /*val textView: TextView = root.findViewById(R.id.text_driver)
+        val textView: TextView = root.findViewById(R.id.text_driver)
         driverViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
-        })*/
-        val myDataset = Datasource().loadAffirmations()
-
-        val recyclerView = root.findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.adapter = ItemAdapter(this, myDataset)
-
-        // Use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true)
+        })
 
         val itemType: String = "New Driver"
         val fab: FloatingActionButton = root.findViewById(R.id.floating_action_btn_1)
